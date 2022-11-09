ba1c586b73f1524584a890ca2c4df2350ec8a5a8
Melvin W <melvinhw@me.com>
2022-11-09 11:24:36 +0000

    Initial Commit

---
+++ ReduxLikeDemo/ReduxLikeDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReduxLikeDemoApp.swift
+//  ReduxLikeDemo
+//
+//  Created by M W on 09/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct ReduxLikeDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
